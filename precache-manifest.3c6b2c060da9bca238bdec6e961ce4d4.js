471117b91c59eb5ba17dbd4912569fe2a3878e09
Nick <web4biz.uk@gmail.com>
2019-06-25 22:06:47 +0100

    Updates

--- precache-manifest.3c6b2c060da9bca238bdec6e961ce4d4.js
+++ precache-manifest.3c6b2c060da9bca238bdec6e961ce4d4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2e851cf204a8663393f25ebb9fe09ee5",
+    "revision": "aa15b535d4d1a6080f13237e8e7909d8",
     "url": "/catch-me-if-you-can/index.html"
   },
   {
-    "revision": "228fb8cbe51e0d0856e0",
+    "revision": "47ee7f1c4021126b2293",
     "url": "/catch-me-if-you-can/static/css/main.560820b6.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/catch-me-if-you-can/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "228fb8cbe51e0d0856e0",
-    "url": "/catch-me-if-you-can/static/js/main.2755a575.chunk.js"
+    "revision": "47ee7f1c4021126b2293",
+    "url": "/catch-me-if-you-can/static/js/main.35b3ee30.chunk.js"
   },
   {
     "revision": "5755eacb39f6795f40a8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/catch-me-if-you-can/precache-manifest.1ca59be3531a9ac0a8505237d540b202.js"
+  "/catch-me-if-you-can/precache-manifest.3c6b2c060da9bca238bdec6e961ce4d4.js"
 );
 
 self.addEventListener('message', (event) => {
